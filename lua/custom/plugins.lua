6cca2319c6af5b171a1c813d92eb6ab69ee94d00
Christopher Wilke <christopher.wilke86@googlemail.com>
2023-11-03 18:13:33 +0100

    added rust cfg

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,3 +1,30 @@
-local plugins = {}
+local plugins = {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "rust-analyzer"
+      }
+    }
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.config.lspconfig"
+    end
+  },
+  {
+    "simrat39/rust-tools.nvim",
+    ft = "rust",
+    dependencies = "neovim/nvim-lspconfig",
+    options = function()
+      return "custom.configs.rust-tools"
+    end,
+    config = function(_, opts)
+      require('rust-tools').setup(opts)
+    end
+  }
+}
 
 return plugins
